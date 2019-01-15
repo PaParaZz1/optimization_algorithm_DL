b16fd4ba419146e2decac5e9b1a416cc5230c601
PaParaZz1 <niuyazhe@buaa.edu.cn>
2019-01-15 23:14:32 +0800

    add result png

--- cifar10_exp/plot.py
+++ cifar10_exp/plot.py
@@ -5,8 +5,8 @@ import numpy as np
 def main(plot_type='accuracy'):
     SGD_l = np.load('experiment/SGD/loss.npy')
     SGD_a = np.load('experiment/SGD/accuracy.npy')
-    #SGD_momentum_l = np.load('experiment/SGD_momentum/loss.npy')
-    #SGD_momentum_a = np.load('experiment/SGD_momentum/accuracy.npy')
+    SGD_momentum_l = np.load('experiment/SGD_momentum/loss.npy')
+    SGD_momentum_a = np.load('experiment/SGD_momentum/accuracy.npy')
     Adagrad_l = np.load('experiment/Adagrad/loss.npy')
     Adagrad_a = np.load('experiment/Adagrad/accuracy.npy')
     RMSprop_l = np.load('experiment/RMSprop/loss.npy')
@@ -16,6 +16,7 @@ def main(plot_type='accuracy'):
     epoch = [x for x in range(100)]
     if plot_type == 'loss':
         plt.plot(epoch, SGD_l, color='red', label='SGD')
+        plt.plot(epoch, SGD_momentum_l, color='green', label='SGD_momentum')
         plt.plot(epoch, Adagrad_l, color='orange', label='Adagrad')
         plt.plot(epoch, RMSprop_l, color='blue', label='RMSprop')
         plt.plot(epoch, Adam_l, color='purple', label='Adam')
@@ -25,6 +26,7 @@ def main(plot_type='accuracy'):
         plt.show()
     elif plot_type == 'accuracy':
         plt.plot(epoch, SGD_a, color='red', label='SGD')
+        plt.plot(epoch, SGD_momentum_a, color='green', label='SGD_momentum')
         plt.plot(epoch, Adagrad_a, color='orange', label='Adagrad')
         plt.plot(epoch, RMSprop_a, color='blue', label='RMSprop')
         plt.plot(epoch, Adam_a, color='purple', label='Adam')
@@ -37,4 +39,5 @@ def main(plot_type='accuracy'):
 
 
 if __name__ == "__main__":
+    #main('loss')
     main('accuracy')
